d548ee1b7775bb373b8f6e5522aba7563e621d1b
SkyThe <88000687+YunruKnowledge@users.noreply.github.com>
2022-10-27 21:12:00 +0200

    +banner img rotate

--- public/js/index_animate_ui.js
+++ public/js/index_animate_ui.js
@@ -5,9 +5,13 @@
 // Observe logic
 const obsrv = new IntersectionObserver((ent)=>{
     ent.forEach((ent)=>{
+        if (ent.isIntersecting && ent.target.classList.contains("banner_image")) bannerImage.style.rotate = "0deg"
         if (ent.isIntersecting) ent.target.classList.add("animate");
     });
 });
 
 const target = document.querySelectorAll("[invisible]");
 target.forEach((target)=> obsrv.observe(target));
+
+const bannerImage = document.getElementById("banner_image");
+obsrv.observe(bannerImage);
